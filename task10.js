083e9f6457536e3c91bfe64f5daf56f43a7d88d3
Umair Sadiq <mohidbuttm22@gmaail.com>
2021-01-12 10:02:09 +0500

    correction in task 10

--- task10.js
+++ task10.js
@@ -1,19 +1,7 @@
 var SortArray = [20,53,78,4,91,12];
 var  a = [];
+let lowestToHighest = SortArray.sort((a, b) => a - b);
+console.log(lowestToHighest);
 
-for (let i = 0; i < SortArray.length; i++) {
-    var b = false;
-    for (let j = i+1; j < SortArray.length; j++) {
-        if (SortArray[i] > SortArray[j]) {
-            console.log(SortArray[j]);
-            var b = true;
-            break;
-        }   
-    } 
-    if (b == false) {
-        a.push(SortArray[i]);
-    } 
-}
-console.log(a);
 
 
