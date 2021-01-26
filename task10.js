0ed7e891d2c49282563f47628553ef71375f11a0
Umair Sadiq <mohidbuttm22@gmaail.com>
2021-01-26 18:19:11 +0500

    Update task10.js

--- task10.js
+++ task10.js
@@ -1,7 +1,24 @@
 var SortArray = [20,53,78,4,91,12];
 var  a = [];
-let lowestToHighest = SortArray.sort((a, b) => a - b);
-console.log(lowestToHighest);
+for (let i = -1; i < SortArray.length; i++) {
+	for (let j = i+1; j < SortArray.length; j++) {
+		var erase = true;
+		if (SortArray[i] > SortArray[j]) {
+		    console.log(SortArray[i]);
+			erase = false;
+			break;
+	}
+	if (erase === false) {
+		a.push(SortArray[i]);
+		}
+}
+			
+}
+	
+	
+
+/*let lowestToHighest = SortArray.sort((a, b) => a - b);
+console.log(lowestToHighest);*/
 
 
 
